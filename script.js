99badbf99b7e9a5751bbebb467a112a0ebd8f75c
apreynolds1989 <apreynolds1989@gmail.com>
2022-03-26 01:05:14 -0230

    Create Function to compare choices and declare winner

--- script.js
+++ script.js
@@ -39,3 +39,42 @@ console.log(playerChoice);
 
 console.log(`Your Choice: ${playerChoice}`);
 console.log(`Computer's Choice: ${computerChoice}`);
+
+// Compare both choices and decalre the winner
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+function compareChoices(choice1, choice2) {
+    if (choice1 === choice2) {
+        ties++;
+        console.log('Tie!');
+    } else if (choice1 === 'rock') {
+        if (choice2 === 'paper') {
+            computerScore++;
+            console.log('Computer Wins!');
+        } else {
+            playerScore++;
+            console.log('You Win!');
+        }
+    } else if (choice1 === 'paper') {
+        if (choice2 === 'scissors') {
+            computerScore++;
+            console.log('Computer Wins!');
+        } else {
+            playerScore++;
+            console.log('You Win!');
+        }
+    } else {
+        if (choice2 === 'rock') {
+            computerScore++;
+            console.log('Computer Wins!');
+        } else {
+            playerScore++;
+            console.log('You Win!');
+        }
+    };
+};
+
+compareChoices(playerChoice, computerChoice);
+console.log(`Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
