5825084d1d34e2bead57fc7a9502fc0bf185335f
apreynolds1989 <apreynolds1989@gmail.com>
2022-03-26 01:20:45 -0230

    Create Function to run Game 5 times. Add alerts in between games to update the score

--- script.js
+++ script.js
@@ -19,9 +19,6 @@ function generateChoice() {
 
 }
 
-generateChoice();
-console.log(computerChoice);
-
 // Take input from player and check to ensure it matches either rock, paper or scissors
 let playerChoice = '';
 let takeInput = () => {
@@ -31,15 +28,6 @@ let takeInput = () => {
     };
 };
 
-takeInput();
-
-console.log(playerChoice);
-
-// Print Both Choices to Console
-
-console.log(`Your Choice: ${playerChoice}`);
-console.log(`Computer's Choice: ${computerChoice}`);
-
 // Compare both choices and decalre the winner
 let playerScore = 0;
 let computerScore = 0;
@@ -48,33 +36,52 @@ let ties = 0;
 function compareChoices(choice1, choice2) {
     if (choice1 === choice2) {
         ties++;
+        alert(`Tie! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
         console.log('Tie!');
     } else if (choice1 === 'rock') {
         if (choice2 === 'paper') {
             computerScore++;
+            alert(`Computer Wins! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('Computer Wins!');
         } else {
             playerScore++;
+            alert(`You Win! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('You Win!');
         }
     } else if (choice1 === 'paper') {
         if (choice2 === 'scissors') {
             computerScore++;
+            alert(`Computer Wins! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('Computer Wins!');
         } else {
             playerScore++;
+            alert(`You Win! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('You Win!');
         }
     } else {
         if (choice2 === 'rock') {
             computerScore++;
+            alert(`Computer Wins! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('Computer Wins!');
         } else {
             playerScore++;
+            alert(`You Win! Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
             console.log('You Win!');
         }
     };
 };
 
-compareChoices(playerChoice, computerChoice);
-console.log(`Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
+// Create a function to the game 5 times, showing the record after each round
+
+function fiveRoundGame(func1, func2, func3) {
+    for (let i = 0; i < 5; i++) {
+        func1();
+        func2();
+        console.log(`Your Choice: ${playerChoice}`);
+        console.log(`Computer's Choice: ${computerChoice}`);
+        func3(playerChoice, computerChoice);
+        console.log(`Current Record (W-L-T): ${playerScore}-${computerScore}-${ties}`);
+    };
+};
+
+fiveRoundGame(generateChoice, takeInput, compareChoices);
